788001ea0deee3003fb054720ec13ce0b0d1da01
yanz17 <dwiantoyan@gmail.com>
2024-07-04 10:31:13 +0700

    menambahkan halaman info produk

--- navbar.php
+++ navbar.php
@@ -21,6 +21,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="../customer/index.php">Dashboard</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../customer/info_produk.php">Info Produk</a>
+                    </li>
                 <?php endif; ?>
                 <li class="nav-item">
                     <a class="nav-link" href="../logout.php">Logout</a>
